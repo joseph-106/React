113085e9ec2aad3fab427f8a42a95672bc73720c
unknown <sjjoseph106@gmail.com>
2021-07-09 21:10:56 +0900

    컴포넌트 제작

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import logo from './logo.svg';
 import React, { Component } from 'react';
+import Subject from './components/Subject.js';
+import TOC from './components/TOC.js';
+import Content from './components/Content.js';
 import './App.css';
 
 class App extends Component {
   render() {
-  return (
-    <div className="App">
-      YEE~
-    </div>
-  );
-}
+    return (
+      <div className="App">
+        <Subject title="WEB" sub="world wide web"></Subject>
+        <Subject title="React" sub="For UI"></Subject>
+        <TOC></TOC>
+        <Content title="HTML" desc="HTML is Hyper Text Markup Language"></Content>
+      </div>
+    );
+  }
 }
 
 export default App;
