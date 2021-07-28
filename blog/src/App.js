e4444a69942d10401af44e0f82787e24c85f78da
unknown <sjjoseph106@gmail.com>
2021-07-28 20:19:27 +0900

    React 기초 마무리

--- blog/src/App.js
+++ blog/src/App.js
@@ -6,41 +6,51 @@ function App(){
  
   let [글제목, 글제목변경] = useState(['첫 글', '두 번째 글', '세번쨰']);
   let [따봉, 따봉변경] = useState(0);
-
-  function 제목바꾸기 () {
-    var newArray = [...글제목];
-    newArray[0] = '글 제목 변경';
-    글제목변경(newArray);
-  }
+  let [modal, modal변경] = useState(false);
+  let [누른제목, 누른제목변경] = useState(0);
+  let [입력값, 입력값변경] = useState('');
 
   return (
     <div className="App">
       <div className="black-nav">
         <div>프로그래밍 잡동사니</div>
       </div>
-      <div className="list">
-        <h3>{ 글제목[0] } <span onClick={ ()=>{따봉변경(따봉+1)} }>👍</span> { 따봉 }</h3>
-        <button onClick={ ()=>{제목바꾸기()} }>버튼</button>
-        <p>7월 19일 발행</p>
-        <hr/>
-        <h3>{ 글제목[1] }</h3>
-        <p>7월 20일 발행</p>
-        <hr/>
-        <h3>{ 글제목[2] }</h3>
-        <p>7월 21일 발행</p>
-        <hr/>
-      </div>
 
-      <Modal></Modal>
+      {
+        글제목.map(function(글, i){
+          return (
+            <div className="list" key={i}>
+              <h3 onClick={ ()=>{누른제목변경(i)}}>{ 글 } <span onClick={ ()=>{따봉변경(따봉+1)} }>👍</span> { 따봉 }</h3>
+              <p>7월 19일 발행</p>
+              <hr/>
+            </div>
+          )
+        })
+      }
+
+      <div className="publish">
+        <input onChange={ (e)=>{ 입력값변경(e.target.value) } }></input>
+        <button onClick={ ()=>{
+          var newArray = [...글제목];
+          newArray.unshift(입력값);
+          글제목변경(newArray)
+        } }>저장</button>
+      </div>
       
+      <button onClick={ ()=>{ modal변경(!modal) } }> 열고닫는버튼 </button>
+      { 
+         modal === true 
+         ? <Modal 글제목={글제목} 누른제목={누른제목} ></Modal>
+         : null
+      }
     </div>
   );
 }
 
-function Modal(){
+function Modal(props){
   return(
     <div className="modal">
-        <h2>제목</h2>
+        <h2>{ props.글제목[props.누른제목] }</h2>
         <p>날짜</p>
         <p>상세내용</p>
       </div>
